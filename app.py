2a386202824cc5d858ba6e3d362d891e25e16515
CRINOT <elvisckcrinot@gmail.com>
2026-04-09 17:16:03 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Configuration de base
+st.set_page_config(page_title="INGCO & GDIZ Hub", layout="wide")
+
+st.title("🌍 Logistique Afrique de l'Ouest")
+st.markdown("---")
+st.success("L'application est en ligne !")
+st.info("👈 Cliquez sur la flèche en haut à gauche pour ouvrir le menu et accéder au module INGCO.")
+
+# Petit message d'accueil
+st.write("Bienvenue Elvis ! Cette plateforme centralise vos solutions logistiques pour la GDIZ et INGCO BÉNIN.")
