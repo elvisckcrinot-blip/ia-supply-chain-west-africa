2c5b58b751396e523c3496711411f16d96d45f2b
CRINOT <elvisckcrinot@gmail.com>
2026-04-11 00:31:05 +0100

    Add prediction and anomaly detection functions

--- models/ai_engine.py
+++ models/ai_engine.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+def prediction_delai_eta(historique_trajets):
+    """
+    Futur emplacement du modèle Random Forest ou XGBoost 
+    pour prédire les retards sur l'axe Malanville.
+    """
+    # Ici viendra le code scikit-learn
+    pass
+
+def detection_anomalies_stock(data_stock):
+    """
+    Identifie les vols ou erreurs de saisie dans l'entrepôt.
+    """
+    # Ici viendra le code Isolation Forest
+    pass
+  
