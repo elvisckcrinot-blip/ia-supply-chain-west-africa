36094d178c7d8cf56788e1eb0c4f6d9a9e4fd70f
CRINOT <elvisckcrinot@gmail.com>
2026-04-11 02:47:21 +0100

    Update mit_calculations.py

--- models/mit_calculations.py
+++ models/mit_calculations.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# --- FONCTIONS WMS (Warehouse) ---
+
 def calcul_wilson_eoq(demande_annuelle, cout_commande, cout_stockage):
     if demande_annuelle <= 0 or cout_stockage <= 0: return 0
     eoq = np.sqrt((2 * demande_annuelle * cout_commande) / cout_stockage)
@@ -7,6 +9,24 @@ def calcul_wilson_eoq(demande_annuelle, cout_commande, cout_stockage):
 
 def calcul_point_de_commande(demande_moyenne, delai_fournisseur, stock_securite):
     return int((demande_moyenne * delai_fournisseur) + stock_securite)
+
+def analyse_pareto_abc(df, colonne_valeur):
+    """
+    Segmentation ABC selon Pareto : indispensable pour le WMS.
+    """
+    df = df.sort_values(by=colonne_valeur, ascending=False)
+    df['cum_sum'] = df[colonne_valeur].cumsum()
+    df['cum_perc'] = 100 * df['cum_sum'] / df[colonne_valeur].sum()
+    
+    def assign_abc(perc):
+        if perc <= 80: return 'A'
+        elif perc <= 95: return 'B'
+        else: return 'C'
+        
+    df['Classe_ABC'] = df['cum_perc'].apply(assign_abc)
+    return df
+
+# --- FONCTIONS TMS (Transport) ---
 
 def calcul_cout_transport(distance, consommation_moyenne, prix_carburant, cout_fixe):
     carburant_total = (distance / 100) * consommation_moyenne
@@ -16,9 +36,6 @@ def calcul_cout_transport(distance, consommation_moyenne, prix_carburant, cout_f
     return int(cout_total), round(emissions_co2, 2)
 
 def calcul_rentabilite_flotte(cout_trajet, tarif_client, camions_jour):
-    """
-    Calcule le gain net annuel pour la flotte sur 360 jours.
-    """
     profit_par_trajet = tarif_client - cout_trajet
     profit_journalier = profit_par_trajet * camions_jour
     profit_annuel = profit_journalier * 360
