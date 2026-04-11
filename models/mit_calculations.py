8071815d8d47ab505245349729e9d4e7ba080cc4
CRINOT <elvisckcrinot@gmail.com>
2026-04-11 01:08:41 +0100

    Refactor functions and update return type in mit_calculations
    Removed docstrings from several functions and updated the return type of 'calcul_cout_transport' to return total cost as an integer.

--- models/mit_calculations.py
+++ models/mit_calculations.py
@@ -1,27 +1,16 @@
 import numpy as np
 
 def calcul_wilson_eoq(demande_annuelle, cout_commande, cout_stockage):
-    """
-    Modèle Wilson (EOQ) : Calcule la quantité économique de commande.
-    Optimise le compromis entre coût de stockage et coût de passation de commande.
-    """
     if demande_annuelle <= 0 or cout_stockage <= 0:
         return 0
     eoq = np.sqrt((2 * demande_annuelle * cout_commande) / cout_stockage)
     return int(eoq)
 
 def calcul_point_de_commande(demande_moyenne, delai_fournisseur, stock_securite):
-    """
-    Modèle ROP (Reorder Point) : Définit quand commander.
-    Formule : (Demande journalière * Délai en jours) + Stock de sécurité.
-    """
     rop = (demande_moyenne * delai_fournisseur) + stock_securite
     return int(rop)
 
 def analyse_pareto_abc(df, colonne_valeur):
-    """
-    Segmentation ABC : Classe les produits par importance financière.
-    """
     df = df.sort_values(by=colonne_valeur, ascending=False)
     df['cum_sum'] = df[colonne_valeur].cumsum()
     df['cum_perc'] = 100 * df['cum_sum'] / df[colonne_valeur].sum()
@@ -36,12 +25,13 @@ def analyse_pareto_abc(df, colonne_valeur):
 
 def calcul_cout_transport(distance, consommation_moyenne, prix_carburant, cout_fixe):
     """
-    Calcule le coût total d'un trajet et l'empreinte CO2.
+    Calcule le coût total en FCFA et l'empreinte CO2.
     """
     carburant_total = (distance / 100) * consommation_moyenne
     cout_carburant = carburant_total * prix_carburant
     cout_total = cout_carburant + cout_fixe
     emissions_co2 = carburant_total * 2.68
     
-    return round(cout_total, 2), round(emissions_co2, 2)
+    # Retourne le coût en entier (FCFA) et le CO2 avec 2 décimales
+    return int(cout_total), round(emissions_co2, 2)
     
